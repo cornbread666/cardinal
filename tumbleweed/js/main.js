37d697f7c4b2d710f6ee0d3fd1273b4919511794
cornbread666 <47578486+cornbread666@users.noreply.github.com>
2024-05-09 17:54:40 -0400

    tumbleweed

---
+++ tumbleweed/js/main.js
@@ -0,0 +1,28 @@
+let PAGE_NUMBER = 0;
+
+intro();
+
+function intro() {
+    document.getElementById("play_triangle").addEventListener("click", intro_blurb);
+    document.getElementById("intro_triangle").addEventListener("click", startQuiz);
+}
+
+function intro_blurb() {
+    document.getElementById("title_page").classList.add("page_fly_out");
+    setTimeout(function() {
+        document.getElementById("intro_page").classList.add("page_fly_in");
+    }, 1001);
+    
+}
+
+function startQuiz() {
+    document.getElementById("intro_page").classList.add("page_fly_out");
+    setTimeout(function() {
+        document.getElementById("page1").classList.add("page_fly_in");
+    }, 1001);
+    
+}
+
+function nextPage() {
+    PAGE_NUMBER = PAGE_NUMBER + 1;
+}
