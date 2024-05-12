7e54a5edcce4ba68d0276911e7dbcfd9b07ac00b
cornbread666 <47578486+cornbread666@users.noreply.github.com>
2024-05-12 00:43:06 -0400

    tumbleweed

--- tumbleweed/js/main.js
+++ tumbleweed/js/main.js
@@ -1,10 +1,10 @@
 let PAGE_NUMBER = 0;
-let dragged;
+let HORSEY = document.getElementById("horse_piece");
 
 intro();
 
 function intro() {
-    document.getElementById("play_triangle").addEventListener("click", intro_blurb);
+    document.getElementById("play_triangle").addEventListener("click", introBlurb);
     document.getElementById("intro_triangle").addEventListener("click", startQuiz);
 
     questionPages = document.getElementsByClassName("answer triangle");
@@ -12,27 +12,57 @@ function intro() {
         questionPages[i].addEventListener("click", nextPage);
     }
 
-    // chess page listeners
-    document.getElementById("horse_piece").addEventListener("dragstart", (event) => {
-        event.dataTransfer.effectAllowed = "move";
-        dragged = event.target;
-    });
-    chessSquares = document.getElementsByClassName("chess_square");
-    for (let i = 0; i < chessSquares.length; i++) {
-        chessSquares[i].addEventListener("dragover", (event) => { event.preventDefault(); });
-        chessSquares[i].addEventListener("drop", (event) => {
-            event.preventDefault();
-            if (event.target.className === "chess_square" && event.target !== dragged) {
-                dragged.parentNode.removeChild(dragged);
-                event.target.appendChild(dragged);
-                document.getElementById("prompt31").style.opacity = "1";
-                nextPage(event);
+    HORSEY.onpointerdown = beginDrag;
+    HORSEY.onpointerup = stopDrag;
+
+    //loadPage(3);
+}
+
+function beginDrag(e) {
+    HORSEY.onpointermove = drag;
+    HORSEY.setPointerCapture(e.pointerId);
+}
+
+function stopDrag(e) {
+    HORSEY.onpointermove = null;
+    HORSEY.releasePointerCapture(e.pointerId);
+    let targetElements = document.elementsFromPoint(e.pageX, e.pageY);
+    let targetFound = false;
+    for (let i = 0; i < targetElements.length; i++) {
+
+        let te = targetElements[i];
+        if (te.classList.contains("chess_square") && te !== HORSEY.parentNode) {
+            targetFound = true;
+            HORSEY.parentNode.removeChild(HORSEY);
+            te.appendChild(HORSEY);
+            HORSEY.style.position = 'relative';
+            HORSEY.style.left = "0";
+            HORSEY.style.top = "0";
+
+            if (te.id === "chess_square12" || te.id === "chess_square14" || te.id === "chess_square21" || te.id === "chess_square41") {
+                document.getElementById("chessmove_prompt").innerText = "nerd...";
             }
-        });
+
+            document.getElementById("chessmove_prompt").style.opacity = "1";
+            nextPage(e);
+        }
+    }
+
+    if (!targetFound) {
+        HORSEY.style.position = 'relative';
+        HORSEY.style.left = "0";
+        HORSEY.style.top = "0";
     }
 }
 
-function intro_blurb() {
+function drag(e) {
+    //e.target.style.transform = `translate(${e.clientX}px)`;
+    e.target.style.position = "absolute";
+    e.target.style.left = `${e.clientX-20}px`;
+    e.target.style.top = `${e.clientY-20}px`;
+}
+
+function introBlurb() {
     document.getElementById("title_page").classList.add("page_fly_out");
     setTimeout(function() {
         document.getElementById("title_page").style.display = "none";
@@ -65,4 +95,13 @@ function nextPage(event) {
         PAGE_NUMBER = PAGE_NUMBER + 1;
         document.getElementById("page" + PAGE_NUMBER.toString()).classList.add("page_fly_in");
     }, 1800);
+}
+
+function loadPage(p) {
+    PAGE_NUMBER = p;
+    document.getElementById("title_page").classList.add("page_fly_out");
+    setTimeout(function() {
+        document.getElementById("title_page").style.display = "none";
+        document.getElementById("page" + PAGE_NUMBER.toString()).classList.add("page_fly_in");
+    }, 1001);
 }
