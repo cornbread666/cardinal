496cf6bf2629279c74e0b1fa4c495601227364cd
cornbread666 <47578486+cornbread666@users.noreply.github.com>
2024-06-06 15:44:03 -0400

    portfolio page

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -2,37 +2,43 @@ import Navbar from "./components/Navbar";
 import Page from "./components/Page";
 import About from "./components/About";
 
+import cardinalGIF from "./assets/cardinal.gif";
+import pixelfightGIF from "./assets/pixelfight.gif";
+import tanglerootGIF from "./assets/tangleroot.gif";
+import tumbleweedGIF from "./assets/tumbleweed.gif";
+import cardinalAppPNG from "./assets/cardinal_app.png";
+
 function App() {
   return (
     <main className="max-w-full flex flex-col justify-center items-center">
       <Navbar />
       <Page
         name={ "Cardinal" }
-        gif={ "src/assets/cardinal.gif" }
+        gif={ cardinalGIF }
         desc={ "Simple and elegant daily puzzle game" }
         link= { "https://cornbread.games" }
       />
       <Page
         name={ "Pixel Fight" }
-        gif={ "src/assets/pixelfight.gif" }
+        gif={ pixelfightGIF }
         desc={ "Zero-player game that uses Chart.js for dynamic data visualization" }
         link= { "https://cornbread.games/pixel-fight" }
       />
       <Page
         name={ "Tangleroot" }
-        gif={ "src/assets/tangleroot.gif" }
+        gif={ tanglerootGIF }
         desc={ "Modular probability calculator designed for Old School RuneScape players" }
         link= { "https://cornbread.games/tangleroot" }
       />
       <Page
         name={ "Tumbleweed (WIP)" }
-        gif={ "src/assets/tumbleweed.gif" }
+        gif={ tumbleweedGIF }
         desc={ "Abstract personality quiz that matches you with a unique tumbleweed generated using p5.js" }
         link= { "https://cornbread.games/tumbleweed" }
       />
       <Page
         name={ "Cardinal: Compass Sudoku" }
-        gif={ "src/assets/cardinal_app.png" }
+        gif={ cardinalAppPNG }
         desc={ "iOS version of the Cardinal daily puzzle game written in Swift" }
         link= { "https://apps.apple.com/us/app/id1524115161?fbclid=IwAR1z3Z3rtt0TYrJrU0cD4M0SAxx6fFPXSZrpeHNwPI48fe448CV-tfTBhp8" }
       />
